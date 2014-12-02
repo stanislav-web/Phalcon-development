d98f3085f745c29418717edb43c8f19752bbfcd8
Stanislav WEB <stanisov@gmail.com>
2014-12-02 04:34:52 +0200

    Global Search library develop start

---
+++ apps/Libraries/GlobalSearch/Exceptions/NullArgumentException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Backend module
+ * @package phalcon.local
+ * @subpackage ${NAMESPACE}
+ * @since PHP >=5.4
+ * @version 1.0
+ * @date 02.12.14
+ * @author Stanislav WEB | Lugansk <stanisov@gmail.com>
+ * @copyright Stanilav WEB
+ * @filesource NullArgumentException.php
+ */
+  
