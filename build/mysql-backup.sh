56899f747e7b16d0f38d2b35422e89f0657ef961
Stanislav WEB <stanisov@gmail.com>
2015-01-04 15:41:48 +0200

    Create Backup Shell script

---
+++ build/mysql-backup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#Define variables
+DATE=$(date +"%m-%d-%Y")
+FILE=/var/www/phalcon-devtools/phalcon.local/backup/phalcon.local-$DATE.sql.gz
+DIR=`dirname "$FILE"`
+SP="/-\|"
+SC=0
+
+# Create backup directory
+if [[ ! -e $DIR ]]; then
+            mkdir $DIR
+            chmod 0777 $DIR
+elif [[ ! -d $DIR ]]; then
+    echo "$DIR already exists but is not a directory" 1>&2
+fi
+
+mysqlbackup() {
+    mysqldump -u root -proot phalcon.local | gzip > $FILE;
+}
+
+spin() {
+   printf "\b${SP:SC++:1}"
+   ((SC == ${#SP})) && SC=0
+}
+endspin() {
+      echo "MySQL backup created."
+}
+
+# Create spinner
+until mysqlbackup; do
+   spin
+   echo "Backing up data to $FILE file, please wait..."
+done
+endspin
