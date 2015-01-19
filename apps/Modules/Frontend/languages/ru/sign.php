8605bb1bf556656bd109f9bd20a3765a982da5c8
Stanislav WEB <stanisov@gmail.com>
2015-01-19 10:38:23 +0200

    Merged hove work

---
+++ apps/Modules/Frontend/languages/ru/sign.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Backend module
+ * @package phalcon.local
+ * @subpackage ${NAMESPACE}
+ * @since PHP >=5.4
+ * @version 1.0
+ * @date 19.01.15
+ * @author Stanislav WEB | Lugansk <stanisov@gmail.com>
+ * @copyright Stanilav WEB
+ * @filesource sign.php
+ */
+  
