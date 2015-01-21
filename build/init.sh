5327d720af5fe6361dc0875717022d985b3ad5f2
StanislavWEB <stanisov@gmail.com>
2015-01-21 20:29:55 +0200

    Auth continue develop

--- build/init.sh
+++ build/init.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+# Error control
+
+set -u;
+set -e;
+
+# Define variables
+
+export SET GIT_BRANCH='Frontend';
+
 sudo service php5-fpm restart
 sudo service nginx restart
 sudo service mysql restart
 cd ../
+git pull origin $GIT_BRANCH
 bower update
+composer self-update
