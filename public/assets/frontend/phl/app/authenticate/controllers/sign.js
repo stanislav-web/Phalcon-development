4bdbb2dd56535ff946050a29b4866b8737e4ab36
StanislavWEB <stanisov@gmail.com>
2015-01-30 18:30:26 +0200

    Register template complete

--- public/assets/frontend/phl/app/authenticate/controllers/sign.js
+++ public/assets/frontend/phl/app/authenticate/controllers/sign.js
@@ -13,9 +13,57 @@
         function ($scope, $rootScope, $location, Authentication, $translatePartialLoader, $splash, ROUTES) {
 
             /**
+             * Form switcher
+             */
+            $scope.loginForm = true;
+            $scope.registerForm = false;
+            $scope.toggle = function(form) {
+
+                if(form === 'loginForm') {
+                    $scope.registerForm = false;
+                    $scope.loginForm = true;
+                }
+                else {
+                    $scope.registerForm = true;
+                    $scope.loginForm = false;
+                }
+            };
+
+            /**
              * Sign to account action
              */
             $scope.sign = function () {
+
+                $scope.dataLoading = true;
+
+                // setup credentials
+                var credentials = {
+                    'login': $scope.login,
+                    'password': $scope.password
+                }
+
+                // call auth service
+                Authentication.login(credentials).then(function (response) {
+
+                    if (response.success) {
+
+                        // close splash window & redirect to account
+                        $splash.close();
+                        $location.path(ROUTES.ACCOUNT);
+
+                    } else {
+                        // return error to show in sign form
+                        $scope.error = response.message;
+                        $scope.dataLoading = false;
+
+                    }
+                });
+            };
+
+            /**
+             * Sign to account action
+             */
+            $scope.register = function () {
 
                 $scope.dataLoading = true;
 
