f1b6d14ad1901d319a228ed35e0c75f9f693de7e
StanislavWEB <stanisov@gmail.com>
2015-04-03 17:58:52 +0300

    [ADD] Minifiers JS / CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp        = require('gulp'),
     concat      = require('gulp-concat-css'),
     minify      = require('gulp-minify-css'),
-    notify      = require('gulp-notify')
-    rename      = require('gulp-rename');
+    notify      = require('gulp-notify'),
+    rename      = require('gulp-rename'),
+    uglify      = require('gulp-uglifyjs');
 
-gulp.task('default', function () {
+gulp.task('default', ['css', 'js']);
+
+gulp.task('css', function () {
     return gulp.src('assets/**/*.css')
         .pipe(concat("styles.css"))
         .pipe(minify())
@@ -13,7 +16,17 @@ gulp.task('default', function () {
         .pipe(notify('Done!'));
 });
 
+gulp.task('js', function () {
+    return gulp.src(['assets/phl/js/*.js', 'assets/phl/app/*.js'])
+        .pipe(uglify('app.min.js', {
+            outSourceMap: true
+        }))
+        .pipe(gulp.dest('assets/phl/'))
+        .pipe(notify('Done!'));
+});
+
 gulp.task('watch', function () {
     // watch files inside and run default
-    gulp.watch('assets/**/*.css', ['default']);
+    gulp.watch('assets/**/*.css', ['css']);
+    gulp.watch(['assets/phl/js/*.js', 'assets/phl/app/*.js'], ['js']);
 });
