f9538586953cb0e04ef8a5116b1e7697d46b1180
StanislavWEB <stanisov@gmail.com>
2015-04-03 14:55:31 +0300

    [ADD] Gulp NPM

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
-var gulp = require('gulp');
+var gulp        = require('gulp'),
+    concat      = require('gulp-concat-css'),
+    minify      = require('gulp-minify-css'),
+    notify      = require('gulp-notify')
+    rename      = require('gulp-rename');
 
-gulp.task('default', function() {
-    // place code for your default task here
+gulp.task('default', function () {
+    return gulp.src('assets/**/*.css')
+        .pipe(concat("styles.css"))
+        .pipe(minify())
+        .pipe(rename("styles.min.css"))
+        .pipe(gulp.dest('assets/phl/'))
+        .pipe(notify('Done!'));
+});
+
+gulp.task('watch', function () {
+    // watch files inside and run default
+    gulp.watch('assets/**/*.css', ['default']);
 });
