600e75f842b31799132444efc53e0ca6c857f5dd
StanislavWEB <stanisov@gmail.com>
2015-04-03 13:26:00 +0300

    [ADD] Structure

--- build/public/assets/backend/js/trash/languages/admin-ua.js
+++
@@ -1,9 +0,0 @@
-/*
- * Translated default messages for the jSocial Mobile.
- * @author Stanislav WEB <stanislav@uplab.ru>
- * Locale: UA (Ukrainian; українська мова)
- */
-var lang = {
-    loadingMessage: "Зачекайте...",
-    pageLoadErrorMessage: "Сторінка не знайдена",
-};
--- build/public/assets/backend/js/trash/languages/ru.js
+++
@@ -1,9 +0,0 @@
-/*
- * Translated default messages for the jSocial Mobile.
- * @author Stanislav WEB <stanislav@uplab.ru>
- * Locale: RU (Russian; русский язык)
- */
-var lang = {
-    loadingMessage: "Загружаю...",
-    pageLoadErrorMessage: "Страница не найдена",
-};
