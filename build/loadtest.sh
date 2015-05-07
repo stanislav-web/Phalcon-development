185a09e8f5d6161cdd19a65c58bd696c79117685
Stanislav WEB <stanisov@gmail.com>
2015-05-08 02:47:05 +0300

    [ADD] Load testing tool

---
+++ build/loadtest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define variables
+export SET URL="http://z95.ru/"
+export SET LOGFILE="/var/www/phalcon.local/logs/ab.log"
+
+# Start test
+
+start() {
+    ab -k -n $1 -c $2 -H 'Accept-Language:ru-RU' $3 > $4 &
+}
+
+# Request wishes
+read -p "Type please how much requests do you wish?: " REQUESTS
+read -p "How many users can perform simultaneous downloads?: " CONCURENCY
+read -p "Press [Enter] to start load testing" START
+
+start $REQUESTS $CONCURENCY $URL $LOGFILE
